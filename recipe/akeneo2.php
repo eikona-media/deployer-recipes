463b3977623067bef995d0743f770dba5e56362e
Timo Müller <t.mueller@eikona-media.de>
2018-10-23 15:38:20 +0200

    added build task

--- recipe/akeneo2.php
+++ recipe/akeneo2.php
@@ -10,7 +10,7 @@
 
 namespace Deployer;
 
-require_once __DIR__.'/deploy/update_shared.php';
+require_once __DIR__ . '/deploy/update_shared.php';
 
 /*
  * Akeneo Configuration
@@ -66,6 +66,26 @@ desc('Execute yarn:compile');
 task('yarn:compile', function () {
     run('cd {{release_path}} && {{bin/yarn}} run webpack');
 });
+
+/**
+ * Akeneo build
+ */
+set('local/bin/php', function () {
+    return runLocally('which php');
+});
+
+set('local/bin/composer', function () {
+    return runLocally('which composer');
+});
+
+set('local/bin/yarn', function () {
+    return runLocally('which yarn');
+});
+
+task('build', function () {
+    runLocally('{{local/bin/php}} {{local/bin/composer}} install --prefer-dist --optimize-autoloader --no-progress --no-scripts');
+    runLocally('{{local/bin/yarn}} install');
+})->desc('Build your project');
 
 /*
  * Akeneo update shared dirs + parameters from repo
