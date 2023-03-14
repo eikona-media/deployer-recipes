2aa92c9a1c84fbe8deb73c644048f8026a292026
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2023-03-14 16:15:40 +0100

    Remove deprecated update_shared_parameters

--- docs/deploy/update_shared.md
+++ docs/deploy/update_shared.md
@@ -15,10 +15,6 @@ require 'recipe/deploy/update_shared.php';
 ```
 
 ```php
-  before('deploy:shared', 'deploy:update_shared_dirs');
-```
-
-```php
   before('deploy:update_shared_dirs', 'deploy:clear_shared_dirs');
 ```
 
@@ -28,25 +24,6 @@ require 'recipe/deploy/update_shared.php';
    * `update_shared_dirs` as array - default: `[]`
 
   Copies all defined dirs from `{{release_path}}/$dir` to `{{deploy_path}}/shared`, to transfer changes from repo to shared folders.
-
-* `deploy:update_shared_parameters` - updates shared parameters for stage with config:
-
-   * `update_shared_parameters_target` file path as string - default: `''`
-   * `update_shared_parameters_source` (optional) can preset `$fileSource` instead of generate from target
-   * `update_shared_parameters_delete` (optional) can preset `$fileDelete` instead of generate from target
-
-  Explodes the file path in `$prefix` and `$extension` - and builds a new path `$fileSource = $prefix.'_{{stage}}.'.$extension;`
-
-  Afterwards the from `{{release_path}}/$fileSource` will be copied to `{{deploy_path}}/shared/{{update_shared_parameters_target}}`, to transfer changes of the stage from repo to shared file.
-
-  Finally, all `$fileDelete = $prefix.'_*';` are deleted.
-
-  >  Example for `.env` files (`.env.ci_{{stage}}`)
-  >  ```php
-  >  set('update_shared_parameters_target', '.env');
-  >  set('update_shared_parameters_source', '.env.ci_{{stage}}');
-  >  set('update_shared_parameters_delete', '.env.*');
-  >  ```
 
 * `clear:update_shared_dirs` - clear shared dirs with config:
   * `clear_shared_dirs` as array - default: `[]`
--- recipe/deploy/update_shared.php
+++ recipe/deploy/update_shared.php
@@ -11,8 +11,6 @@ declare(strict_types=1);
 
 namespace Deployer;
 
-use Deployer\Exception\ConfigurationException;
-
 desc('Update shared dirs from repo');
 task(
     'deploy:update_shared_dirs',
@@ -27,64 +25,6 @@ task(
             if (test("[ -d $(echo {{release_path}}/$dir) ]")) {
                 run("cp -rv {{release_path}}/$dir $sharedPath/".\dirname(parse($dir)));
             }
-        }
-    }
-)->hidden();
-
-desc('Update shared parameters for stage from repo');
-task(
-    'deploy:update_shared_parameters',
-    function () {
-        try {
-            $fileTarget = get('update_shared_parameters_target');
-        } catch (ConfigurationException) {
-            $fileTarget = null;
-        }
-
-        if (empty($fileTarget)) {
-            writeln(
-                '<comment>Configuration "update_shared_parameters_target" not set</comment>'
-            );
-
-            return;
-        }
-
-        try {
-            $fileSource = get('update_shared_parameters_source');
-        } catch (ConfigurationException) {
-            $fileSource = null;
-        }
-        try {
-            $fileDelete = get('update_shared_parameters_delete');
-        } catch (ConfigurationException) {
-            $fileDelete = null;
-        }
-
-        if (empty($fileSource) || null === $fileDelete) {
-            $filePieces = explode('.', $fileTarget);
-            $prefix = implode('.', \array_slice($filePieces, 0, -1));
-            $extension = end($filePieces);
-            if (empty($fileSource)) {
-                $fileSource = $prefix.'_'.get('labels', [])['stage'] ?? ''.$extension;
-            }
-            if (null === $fileDelete) {
-                $fileDelete = $prefix.'_*';
-            }
-        }
-
-        if (!test("[ -f {{release_path}}/$fileSource ]") && output()->isVerbose()) {
-            writeln('<comment>Source parameters "'.$fileSource.'" for stage not found</comment>');
-
-            return;
-        }
-
-        $sharedPath = '{{deploy_path}}/shared';
-        $sudo = get('clear_use_sudo') ? 'sudo' : '';
-
-        run("$sudo cp -rv {{release_path}}/$fileSource $sharedPath/$fileTarget");
-
-        if (!empty($fileDelete)) {
-            run("$sudo rm -rf {{release_path}}/$fileDelete");
         }
     }
 )->hidden();
